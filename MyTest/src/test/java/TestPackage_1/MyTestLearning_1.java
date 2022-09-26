1d937fe3bb37689f3c1b2ddf804f5e11095ef08b
fahim <fahim@aklaptop>
2022-09-25 22:39:37 -0500

    First commit

---
+++ MyTest/src/test/java/TestPackage_1/MyTestLearning_1.java
@@ -0,0 +1,13 @@
+package TestPackage_1;
+
+public class MyTestLearning_1 {
+
+	public void MyMethod () {System.out.println("test");
+	}
+	public static void main(String[] args) {
+		
+		MyTestLearning_1 obj = new MyTestLearning_1();
+		obj.MyMethod();
+	}
+
+}
